ea597e09658ff57059521f19aae24a3c98bbde45
Mike Wilcox <mwilcox56@gmail.com>
2015-11-07 21:17:54 -0500

    add initial mopidy support

--- MusicApp/index.ios.js
+++ MusicApp/index.ios.js
@@ -11,6 +11,49 @@ var {
   Text,
   View,
 } = React;
+var Mopidy = require("mopidy");
+
+console.log('mopidy', Mopidy);
+
+var mopidy = new Mopidy({
+  webSocketUrl: "ws://127.0.0.1:6680/mopidy/ws/"
+});
+
+mopidy.on(console.log.bind(console));
+
+var trackDesc = function (track) {
+    return track.name + " by " + track.artists[0].name +
+        " from " + track.album.name;
+};
+
+var queueAndPlay = function (playlistNum, trackNum) {
+    playlistNum = playlistNum || 0;
+    trackNum = trackNum || 0;
+
+    console.log(mopidy)
+
+    mopidy.getUriSchemes().then(function(schemes) {
+      console.log('schemes:', schemes);
+    });
+
+    mopidy.playlists.getPlaylists().then(function (playlists) {
+        var playlist = playlists[2];
+        console.log("Loading playlist:", playlists, playlist.name, playlist.tracks);
+        return mopidy.tracklist.add(playlist.tracks).then(function (tlTracks) {
+            return mopidy.playback.play(tlTracks[trackNum]).then(function () {
+                return mopidy.playback.getCurrentTrack().then(function (track) {
+                    console.log("Now playing:", trackDesc(track));
+                });
+            });
+            console.log('hi!', tlTracks)
+        });
+    })
+    .catch(console.error.bind(console)) // Handle errors here
+    .done();                            // ...or they'll be thrown here
+};
+
+
+mopidy.on("state:online", queueAndPlay);
 
 var MusicApp = React.createClass({
   render: function() {
